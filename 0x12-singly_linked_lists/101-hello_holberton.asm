5735ef5364c46f8c5e372b846aec8806cd47918b
ShimwatiMoody <frankmoody107@gmail.com>
2023-07-31 08:17:01 -0700

    101-hello_holberton.asm git commit -m

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,10 @@
-section .data
-    hello db "Hello, Holberton,", 0  ; Null-terminated string to be printed
-
-section .text
-    global main
-    extern printf
-
+   global    main
+          extern    printf
 main:
-    push rbp
-    mov rdi, hello       ; Load the address of the string into rdi (first argument)
-    xor rax, rax         ; Clear rax to indicate no floating-point arguments
-    call printf          ; Call the printf function to print the string
-    pop rbp
-    ret                  ; Return from the main function
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
